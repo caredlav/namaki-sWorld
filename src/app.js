eeea942ae99f5a58e8b8e5d6538fe2af7cc2bbc1
caredlav <caredlav@gmail.com>
2022-03-18 17:33:59 -0500

    Se hacen estilos css para el home. Comenzando estilos responsive version mobile.

--- src/app.js
+++ src/app.js
@@ -6,7 +6,11 @@ const routerHome=require('./routes/home.routes');
 
 app.set('views',path.resolve(__dirname,"./views"));
 
+//configuramos la carpeta public para que sea publica
+app.use(express.static('public'));
+
+//declaramos las rutas
 app.use('/',routerHome);
 
-
+//se levanta el servidor en el puerto 3000
 app.listen(3000,console.log('corriendo en puerto 3000'));
