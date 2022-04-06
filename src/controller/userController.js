e7d47e303b0314f189def0bcff8686a46110779d
caredlav <caredlav@gmail.com>
2022-04-06 18:07:46 -0500

    Ligeros cambios en el Home. Termine estilos para el registro de usuarios, incluyendo vista pc. Cree nueva ruta para login de usuarios y finalice los estilos respectivos para vista mobile, tablet y pc.

--- src/controller/userController.js
+++ src/controller/userController.js
@@ -3,6 +3,10 @@ const path=require("path");
 const userController={
     getRegister: (req,res)=>{
         res.sendFile(path.resolve(__dirname,"../views/users/register.html"));
+    },
+
+    getLogin: (req,res)=>{
+        res.sendFile(path.resolve(__dirname,"../views/users/login.html"));
     }
 }
 
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -4,4 +4,6 @@ const userController = require('../controller/userController');
 
 router.get("/registro", userController.getRegister);
 
+router.get("/login",userController.getLogin);
+
 module.exports=router;
