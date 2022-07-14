03a8249e75f2886b2253d4b39085bdf02cada922
caredlav <caredlav@gmail.com>
2022-07-14 16:04:42 -0500

    Se crean los html para el resto de los productos de la categoria anime, se incluyen imagenes y se crean las rutas respectivas. Cambio en nombre de clases y se inicia el proceso para el efecto de pasar el cursor por encima de la imagen del producto.

--- src/controller/productController.js
+++ src/controller/productController.js
@@ -10,6 +10,18 @@ const productController={
     },
     getAnimeFigures: (req,res)=>{
         res.sendFile(path.resolve(__dirname,"../views/products/animeFigures.html"));
+    },
+    getAnimeResins: (req,res)=>{
+        res.sendFile(path.resolve(__dirname,"../views/products/animeResins.html"));
+    },
+    getAnimeMangas: (req,res)=>{
+        res.sendFile(path.resolve(__dirname,"../views/products/animeMangas.html"));
+    },
+    getAnimeAccessories: (req,res)=>{
+        res.sendFile(path.resolve(__dirname,"../views/products/animeAccessories.html"));
+    },
+    getAnimeClothes: (req,res)=>{
+        res.sendFile(path.resolve(__dirname,"../views/products/animeClothes.html"));
     }
 }
 
--- src/routes/product.routes.js
+++ src/routes/product.routes.js
@@ -5,5 +5,9 @@ const productsController = require("../controller/productController");
 router.get("/categorias",productsController.getCategories);
 router.get("/categorias/anime",productsController.getSubCategoriesForAnime);
 router.get("/categorias/anime/figuras",productsController.getAnimeFigures);
+router.get("/categorias/anime/resinas",productsController.getAnimeResins);
+router.get("/categorias/anime/mangas",productsController.getAnimeMangas);
+router.get("/categorias/anime/accesorios",productsController.getAnimeAccessories);
+router.get("/categorias/anime/ropa",productsController.getAnimeClothes);
 
 module.exports=router;
